cd44af0721256f9e72253ed307e0b80f15af7312
Mark Poepping <mark.poepping@admiralcloud.com>
2023-03-09 19:15:35 +0100

    [master] test(App): Added first test | MP
    First test
    Related issues:
    - undefined/browse/master

--- test/test.js
+++ test/test.js
@@ -1,7 +1,30 @@
-//const awsSecrets = require('../index')
+const { expect } = require('chai')
+const aclog = require('../index')
+
+var intercept = require("intercept-stdout");
+
+var captured_text = "";
+
+var unhook_intercept = intercept(function(text) {
+  captured_text += text;
+});
 
 describe('Tests', () => {
-  it('Should work', done => {
-    return done()
+
+
+  it('Check bootstrapInfo', () => {
+    aclog().bootstrapInfo({ appName: 'myApp', branch: 'myBranch' })
+    const lines = captured_text.split('\n')
+    expect(lines[5]).to.contain('Time')
+    expect(lines[6]).to.contain('Environment')
+    expect(lines[7]).to.contain('Branch')
+    expect(lines[7]).to.contain('myBranch')
+    expect(lines[8]).to.contain('Version')
+    expect(lines[9]).to.contain('AppName')
+    expect(lines[9]).to.contain('myApp')
+  })
+
+  it('Unhook', () => {
+    unhook_intercept()
   })
 })
